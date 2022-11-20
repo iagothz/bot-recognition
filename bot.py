1f57f12712955a3bad0ed0615d0cd1772e9a65e0
Iago <iago.formes.r@gmail.com>
2022-11-20 14:17:20 +0000

    modified:   bot.py 	new file:   color.py

--- bot.py
+++ bot.py
@@ -1,1 +1,30 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api
+import win32con
 
+time.sleep(2)
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32api.MOUSEEVENTF_LEFTDOWN,0,0)
+    win32api.mouse_event(win32api.MOUSEEVENTF_LEFTUP,0,0)
+
+while keyboard.is_pressed('q') == False:
+    pic = pyautogui.screenshot(region=(394,288,603,423))
+
+    width, height = pic.size
+
+    for x in range(0,width,5):
+        for y in range (0, height, 5):
+            #Cores do centro: (255, 219, 195)
+            r,g,b = pic.getpixel((x,y))
+
+            if b == 195 and r == 255 and g == 219:
+                if b == 195:
+                    click(x+394,y+288)
+                    time.sleep(0.05)
+                    break
